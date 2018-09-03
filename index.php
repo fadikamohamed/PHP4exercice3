00e2de9e1a2d1b660d7fd7b0d35ee56423ff8851
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 15:00:04 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 3</title>
+  </head>
+  <body>
+    <p>
+    <?php
+      function twoStringFunction($fistString, $secondString) {
+        echo $fistString . ' ' . $secondString;
+      }
+      twoStringFunction('Premiere chaine de caracteres', 'Deuxieme chaine de caracteres');
+     ?>
+     <p>
+  </body>
+</html>
